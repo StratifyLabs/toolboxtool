5a2452cb4f7eb43fb19fff680bf840fd036e0da6
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-11-20 23:09:46 -0700

    display testing

--- src/test/DisplayTest.cpp
+++ src/test/DisplayTest.cpp
@@ -10,8 +10,10 @@ DisplayTest::DisplayTest() : Test(arg::Name("DisplayTest")){
 bool DisplayTest::execute_class_api_case(){
 
 
+
 	DisplayDevice display;
 
+	print_case_message("initialize display");
 	if( display.initialize(
 			 arg::SourceFilePath("/dev/display0")
 			 ) < 0
@@ -19,8 +21,11 @@ bool DisplayTest::execute_class_api_case(){
 		print_case_failed("failed to initialize the display");
 	}
 
-
+	print_case_message("clear display");
 	display.clear();
+	if( display.return_value() < 0 ){
+		print_case_failed("Failed to clear display");
+	}
 
 
 	return case_result();
