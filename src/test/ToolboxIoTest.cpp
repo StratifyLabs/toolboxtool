a9e214321115cdfb8781ee61edeaf23280466b4f
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-12-23 12:30:17 -0700

    IO debugging

--- src/test/ToolboxIoTest.cpp
+++ src/test/ToolboxIoTest.cpp
@@ -1,6 +1,7 @@
 
 #include <ToolboxAPI/toolbox.hpp>
 #include <sapi/hal.hpp>
+#include <sapi/chrono.hpp>
 
 #include "ToolboxIoTest.hpp"
 
@@ -12,20 +13,32 @@ bool ToolboxIoTest::execute_class_api_case(){
 
    IoAttributes attributes;
 
+   attributes.set_flags(Io::enable_vdd_out);
+   TEST_THIS_ASSERT(bool, Io::set_attributes( attributes ) < 0, false);
+
+
    attributes
          .set_peripheral_port(Io::pio_port)
-         .set_pin(Io::pin0)
+         .set_pin(Io::pin1)
          .set_flags(Io::set_io_function | Io::is_output);
 
    Pin pin0(
             Pin::Port(0),
-            Pin::Number(0)
+            Pin::Number(1)
             );
 
    TEST_THIS_ASSERT(bool, pin0.open() < 0, false);
    TEST_THIS_ASSERT(bool, Io::set_attributes( attributes ) < 0, false);
    TEST_THIS_ASSERT(bool, pin0.set_output() < 0, false);
 
+   Timer t;
+   t.start();
+   while(t < Seconds(20) ){
+      pin0 = true;
+      wait(Seconds(2));
+      pin0 = false;
+      wait(Seconds(2));
+   }
 
    return case_result();
 }
